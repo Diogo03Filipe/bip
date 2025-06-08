f3383b116d1d5c719ba18d35ef45e3e59a3b7b87
Diogo Filipe Gonçalves Silva <a57162@alunos.ipb.pt>
2025-06-08 12:19:31 +0100

    a few more changes

--- src/components/Gastronomy/Borscht/Borscht.js
+++ src/components/Gastronomy/Borscht/Borscht.js
@@ -33,33 +33,33 @@ const Borscht = () => {
         <div className='borscht-description'>{translations[language].Borscht.description}</div>
       </div>
       <div className='borscht-ingredients-rect'>
-        <div className='borscht-ingredients-text'>{translations[language].BeanPastries.title2}</div>
-        <div className='borscht-ingredients'>{translations[language].BeanPastries.text}</div>
-        <div className='borscht-ingredients'>{translations[language].BeanPastries.text2}</div>
-        <div className='borscht-ingredients'>{translations[language].BeanPastries.text3}</div>
-        <div className='borscht-ingredients'>{translations[language].BeanPastries.text4}</div>
-        <div className='borscht-ingredients'>{translations[language].BeanPastries.text5}</div>
-        <div className='borscht-ingredients'>{translations[language].BeanPastries.text6}</div>
-        <div className='borscht-ingredients'>{translations[language].BeanPastries.text7}</div>
-        <div className='borscht-ingredients'>{translations[language].BeanPastries.text8}</div>
-        <div className='borscht-ingredients'>{translations[language].BeanPastries.text9}</div>
-        <div className='borscht-ingredients'>{translations[language].BeanPastries.text10}</div>
-        <div className='borscht-ingredients'>{translations[language].BeanPastries.text11}</div>
+        <div className='borscht-ingredients-text'>{translations[language].Borscht.title2}</div>
+        <div className='borscht-ingredients'>{translations[language].Borscht.text}</div>
+        <div className='borscht-ingredients'>{translations[language].Borscht.text2}</div>
+        <div className='borscht-ingredients'>{translations[language].Borscht.text3}</div>
+        <div className='borscht-ingredients'>{translations[language].Borscht.text4}</div>
+        <div className='borscht-ingredients'>{translations[language].Borscht.text5}</div>
+        <div className='borscht-ingredients'>{translations[language].Borscht.text6}</div>
+        <div className='borscht-ingredients'>{translations[language].Borscht.text7}</div>
+        <div className='borscht-ingredients'>{translations[language].Borscht.text8}</div>
+        <div className='borscht-ingredients'>{translations[language].Borscht.text9}</div>
+        <div className='borscht-ingredients'>{translations[language].Borscht.text10}</div>
+        <div className='borscht-ingredients'>{translations[language].Borscht.text11}</div>
       </div>
       <div className='borscht-instructions-rect'>
-        <div className='borscht-instructions-text'>{translations[language].BeanPastries.title3}</div>
-        <div className='borscht-instructions'>{translations[language].BeanPastries.text12}</div>
-        <div className='borscht-instructions'>{translations[language].BeanPastries.text13}</div>
-        <div className='borscht-instructions'>{translations[language].BeanPastries.text14}</div>
-        <div className='borscht-instructions'>{translations[language].BeanPastries.text15}</div>
-        <div className='borscht-instructions'>{translations[language].BeanPastries.text16}</div>
-        <div className='borscht-instructions'>{translations[language].BeanPastries.text17}</div>
+        <div className='borscht-instructions-text'>{translations[language].Borscht.title3}</div>
+        <div className='borscht-instructions'>{translations[language].Borscht.text12}</div>
+        <div className='borscht-instructions'>{translations[language].Borscht.text13}</div>
+        <div className='borscht-instructions'>{translations[language].Borscht.text14}</div>
+        <div className='borscht-instructions'>{translations[language].Borscht.text15}</div>
+        <div className='borscht-instructions'>{translations[language].Borscht.text16}</div>
+        <div className='borscht-instructions'>{translations[language].Borscht.text17}</div>
         
-        <div className='borscht-tips-text'>{translations[language].BeanPastries.title4}</div>
-        <div className='borscht-tips'>{translations[language].BeanPastries.text18}</div>
-        <div className='borscht-tips'>{translations[language].BeanPastries.text19}</div>
-        <div className='borscht-tips'>{translations[language].BeanPastries.text20}</div>
-        <div className='borscht-tips'>{translations[language].BeanPastries.text21}</div>
+        <div className='borscht-tips-text'>{translations[language].Borscht.title4}</div>
+        <div className='borscht-tips'>{translations[language].Borscht.text18}</div>
+        <div className='borscht-tips'>{translations[language].Borscht.text19}</div>
+        <div className='borscht-tips'>{translations[language].Borscht.text20}</div>
+        <div className='borscht-tips'>{translations[language].Borscht.text21}</div>
       </div>
     </>
   );
